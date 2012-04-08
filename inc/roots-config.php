be6a30992d470e91593422efb9c14f909bebea5d
Jeff Uyeno <jeff.uyeno@gmail.com>
2012-04-08 13:11:10 -0700

    add lessphp support for development

--- inc/roots-config.php
+++ inc/roots-config.php
@@ -8,3 +8,4 @@ define('MAIN_CLASSES',         'span8');
 define('SIDEBAR_CLASSES',      'span4');
 define('FULLWIDTH_CLASSES',    'span12');
 define('GOOGLE_ANALYTICS_ID',  '');
+define('ENV_DEVELOPMENT', true);
